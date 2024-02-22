824510dde0ee6e9551cf3447c6310cc8987eaf3f
Sharp0802 <sharp0802.business@gmail.com>
2024-02-23 03:27:57 +0900

    Fix: Use GL error code instead of GLFW error code

--- inc/GL/stdexcept.h
+++ inc/GL/stdexcept.h
@@ -6,14 +6,28 @@
 
 struct gl_category final : std::error_category
 {
+	[[nodiscard]]
     const char* name() const noexcept override
     {
         return "gl_category";
     }
 
+	[[nodiscard]]
     std::string message(const int code) const override
     {
-        return reinterpret_cast<const char*>(glewGetErrorString(code));
+		switch (code)
+		{
+		case GL_NO_ERROR:                      return "GL_NO_ERROR";
+		case GL_INVALID_ENUM:                  return "GL_INVALID_ENUM";
+		case GL_INVALID_VALUE:                 return "GL_INVALID_VALUE";
+		case GL_INVALID_OPERATION:             return "GL_INVALID_OPERATION";
+		case GL_STACK_OVERFLOW:                return "GL_STACK_OVERFLOW";
+		case GL_STACK_UNDERFLOW:               return "GL_STACK_UNDERFLOW";
+		case GL_OUT_OF_MEMORY:                 return "GL_OUT_OF_MEMORY";
+		case GL_TABLE_TOO_LARGE:               return "GL_TABLE_TOO_LARGE";
+		case GL_INVALID_FRAMEBUFFER_OPERATION: return "GL_INVALID_FRAMEBUFFER_OPERATION";
+		default:                               return "Unknown Error";
+		}
     }
 };
 
