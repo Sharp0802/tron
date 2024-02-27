a7a97eeab58db0c989ebf5fb3d37152a7afd21ac
Sharp0802 <sharp0802.business@gmail.com>
2024-02-28 00:13:11 +0900

    Feat: Implicit includes

--- inc/shader.h
+++ inc/shader.h
@@ -3,12 +3,7 @@
 
 #include <GL/glew.h>
 
-#include <glm/vec2.hpp>
-#include <glm/vec3.hpp>
-#include <glm/vec4.hpp>
-#include <glm/mat2x2.hpp>
-#include <glm/mat3x3.hpp>
-#include <glm/mat4x4.hpp>
+#include <glm/glm.hpp>
 
 #include "resource.h"
 
