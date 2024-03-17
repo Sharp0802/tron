12f7262dd6e656c552838ecb776103acfd6b4e84
Sharp0802 <sharp0802.business@gmail.com>
2024-03-17 18:41:34 +0900

    Feat: Declare window abstraction class

---
+++ inc/sys/window.h
@@ -0,0 +1,16 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+namespace tron::sys
+{
+	class Window
+	{
+		int m_width;
+		int m_height;
+
+	public:
+		Window(int w, int h);
+	};
+}
+
+#endif
---
+++ src/sys/window.cxx
@@ -0,0 +1,10 @@
+#include "sys/window.h"
+
+namespace tron::sys
+{
+	Window::Window(int w, int h)
+		: m_width(w), m_height(h)
+	{
+	}
+
+}
