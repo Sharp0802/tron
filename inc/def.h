69e0d0a46b4ac5003739d5de7caba661b1fd1a41
Sharp0802 <sharp0802.business@gmail.com>
2024-03-18 08:42:27 +0900

    Fix: Fix preprocessors

--- inc/def.h
+++ inc/def.h
@@ -1,11 +1,10 @@
 #ifndef TRON_DEF_H
 #define TRON_DEF_H
 
-// TODO: Fix preprocessor
-#if TRON_RELEASE
-#define ASSERT(a)
-#else
+#if _DEBUG
 #define ASSERT(a) assert(a)
+#else
+#define ASSERT(a)
 #endif
 
 namespace tron
