939b94d4cd51fdc34a85f8122b60ff80ee7ed12a
Sharp0802 <sharp0802.business@gmail.com>
2024-04-05 22:38:57 +0900

    Feat: Support OpenGL ES 3.1

--- tron/src/oop/sys/window.cxx
+++ tron/src/oop/sys/window.cxx
@@ -60,14 +60,18 @@ namespace tron::oop::sys
             throw std::runtime_error("Couldn't start glfw3");
         }
 
-        /*
-         * To support macOS, We should use OpenGL 3.2 core profile.
-         * This let macOS use OpenGL 4.
-         */
+#ifdef USE_OPENGL_ES
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE);
+#else
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 
         glfwWindowHint(GLFW_SAMPLES, 4);
 
