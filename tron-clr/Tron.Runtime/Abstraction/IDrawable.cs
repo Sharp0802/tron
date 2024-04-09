17ec374d482752d8098723519e6fae2bbb750f14
Sharp0802 <sharp0802.business@gmail.com>
2024-04-09 14:31:03 +0900

    Feat: Abstract drawable object

---
+++ tron-clr/Tron.Runtime/Abstraction/IDrawable.cs
@@ -0,0 +1,12 @@
+namespace Tron.Runtime.Abstraction;
+
+/// <summary>
+/// A drawable object.
+/// </summary>
+public interface IDrawable
+{
+    /// <summary>
+    /// Draw object on current context.
+    /// </summary>
+    public void Draw();
+}
